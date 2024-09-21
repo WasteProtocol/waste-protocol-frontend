299d931f270cf817c98337680a775b6ceb6d33d4
Ikhalas Mannoon <ikhalas.work@gmail.com>
2024-09-22 04:48:48 +0800

    fix world verify callback

--- src/sections/approve/views/approve-view.tsx
+++ src/sections/approve/views/approve-view.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import type { ISuccessResult } from '@worldcoin/idkit';
+import { useState, useEffect, useCallback } from 'react';
+import { useIDKit, IDKitWidget, VerificationLevel } from '@worldcoin/idkit';
 
 import Card from '@mui/material/Card';
 import Table from '@mui/material/Table';
@@ -12,6 +14,7 @@ import { useBoolean } from 'src/hooks/use-boolean';
 
 import axios from 'src/utils/axios';
 
+import { verify } from 'src/actions/verify';
 import { HOST_API } from 'src/config-global';
 
 import Scrollbar from 'src/components/scrollbar';
@@ -43,6 +46,16 @@ const TABLE_HEAD = [
 // ----------------------------------------------------------------------
 
 export default function ApproveView() {
+  const app_id = process.env.NEXT_PUBLIC_WLD_APP_ID as `app_${string}`;
+  const action = process.env.NEXT_PUBLIC_WLD_ACTION;
+
+  if (!app_id) {
+    throw new Error('app_id is not set in environment variables!');
+  }
+  if (!action) {
+    throw new Error('action is not set in environment variables!');
+  }
+
   const init = useBoolean(true);
   const table = useTable();
   const [tableData, setTableData] = useState<[]>([]);
@@ -50,6 +63,24 @@ export default function ApproveView() {
   const forceReload = useBoolean();
   const approve = useBoolean();
   const notFound = !tableData.length;
+
+  const [seletedTradeId, setSeletedTradeId] = useState<number | null>(null);
+
+  const { setOpen } = useIDKit();
+
+  const onSuccess = useCallback(() => {
+    if (seletedTradeId) {
+      onApproveRow(seletedTradeId);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [seletedTradeId]);
+
+  const handleProof = async (result: ISuccessResult) => {
+    const data = await verify(result);
+    if (!data.success) {
+      throw new Error(`Verification failed: ${data.detail}`);
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -68,11 +99,17 @@ export default function ApproveView() {
       approve.onTrue();
 
       await axios.get(`${HOST_API}/trades/approve/${tradeId}`);
+      forceReload.onToggle();
     } catch (error) {
       console.log(error);
     } finally {
       approve.onFalse();
     }
+  };
+
+  const setOpenWorld = (tId: number) => {
+    setOpen(true);
+    setSeletedTradeId(tId);
   };
 
   return (
@@ -99,7 +136,7 @@ export default function ApproveView() {
 
               <TableBody>
                 {tableData.map((row: any) => (
-                  <ApproveTableRow key={row.id} row={row} onApproveRow={onApproveRow} />
+                  <ApproveTableRow key={row.id} row={row} setOpenWorld={setOpenWorld} />
                 ))}
 
                 <TableEmptyRows
@@ -113,6 +150,14 @@ export default function ApproveView() {
           </Scrollbar>
         </TableContainer>
       </Card>
+
+      <IDKitWidget
+        action={action}
+        app_id={app_id}
+        onSuccess={onSuccess}
+        handleVerify={handleProof}
+        verification_level={VerificationLevel.Device}
+      />
     </Container>
   );
 }
